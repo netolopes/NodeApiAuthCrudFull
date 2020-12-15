11bba42b4c1a0e7e40bd80b38c17f3a1d61316ae
netolopes <netolopes27@gmail.com>
2020-12-15 14:43:49 -0300

    conclusao cors/avatar/includes na api

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 import * as Yup from 'yup';
 
@@ -17,6 +18,13 @@ class SessionController {
     const { email, password } = req.body;
     const user = await User.findOne({
       where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -26,12 +34,14 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'passowrd does not match' });
     }
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
 
       token: jwt.sign({ id }, authConfig.secret, {
